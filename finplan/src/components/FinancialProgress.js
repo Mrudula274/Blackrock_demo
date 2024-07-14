ecb26e78cb2c0120341279742e56d1dc55accb87
Mrudula Wakodkar <wakodkarmrudula27@gmail.com>
2024-07-14 23:00:50 +0530

    updated

---
+++ finplan/src/components/FinancialProgress.js
@@ -0,0 +1,14 @@
+// src/components/FinancialProgress.js
+import React from 'react';
+import './FinancialProgress.css';
+
+const FinancialProgress = () => {
+  return (
+    <div className="financial-progress">
+      <h2>Financial progress</h2>
+      {/* Progress list here */}
+    </div>
+  );
+};
+
+export default FinancialProgress;
